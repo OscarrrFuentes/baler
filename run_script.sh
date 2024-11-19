052c3c4b2253a6922fd8c372802184ff599c9f02
Oscar Fuentes Rebato <ofrebato@wn3801030.hep.manchester.ac.uk>
2024-11-19 16:28:35 +0000

    Got it to run on Condor succesfully

--- run_script.sh
+++ run_script.sh
@@ -1,56 +1,33 @@
 #!/bin/bash
+
+BATCH_SIZE=$1
+
 export PATH="/gluster/home/ofrebato/.cache/pypoetry/virtualenvs/baler-O7HYjMIZ-py3.9/bin:/gluster/home/ofrebato/.local/bin:/bin:/gluster/home/ofrebato/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/puppetlabs/bin:$PATH"
 echo $PATH
 
-# Python setup
-# python -m ensurepip --upgrade
-# python -m pip install --upgrade pip
-# pip install ipython ipykernel 
-# pip install numpy matplotlib torch scikit-learn tqdm numba pytest hls4ml
-# python -m pip install --user pipx
-# python -m pipx ensurepath
-# pipx install poetry
-# poetry config virtualenvs.options.system-site-packages true
-
 # poetry environment
 mkdir -p .cache/pypoetry/virtualenvs/
-
-# baler
-cp -r /gluster/home/ofrebato/baler baler
 cp -r /gluster/home/ofrebato/.cache/pypoetry/virtualenvs/baler-O7HYjMIZ-py3.9 .cache/pypoetry/virtualenvs/baler-O7HYjMIZ-py3.9
-cd baler
 
-# logging
-touch job.out
-touch job.err
-touch job.log
-touch job.all_output
-echo "hello world" > job.all_output
+# Copy baler
+cp -r /gluster/home/ofrebato/baler baler_${BATCH_SIZE}
+cd baler_${BATCH_SIZE}
 
-#copy results
-cp job.out /gluster/home/ofrebato/baler/job.out
-cp job.err /gluster/home/ofrebato/baler/job.err
-cp job.log /gluster/home/ofrebato/baler/job.log
-cp job.all_output /gluster/home/ofrebato/job.all_output
+# Set up Poetry to use scratch directory
+export POETRY_HOME="$PWD/.poetry"
+export POETRY_VIRTUALENVS_PATH="$PWD/.venvs"
+export POETRY_VIRTUALENVS_IN_PROJECT=true
 
-# poetry
-poetry install
+# Install Poetry and dependencies
+poetry install --no-root
 
-#copy results
-cp job.out /gluster/home/ofrebato/baler/job.out
-cp job.err /gluster/home/ofrebato/baler/job.err
-cp job.log /gluster/home/ofrebato/baler/job.log
-cp job.all_output /gluster/home/ofrebato/job.all_output
+# Run for a given batch size
+python run_multiple.py -n 2 -s run_multiple_results/result_${BATCH_SIZE}.npz -b ${BATCH_SIZE} > /gluster/home/ofrebato/baler/job_${BATCH_SIZE}.out 2>&1
 
-# run
-python run_multiple.py -n 1 -s
+# Copy results to home directory
+cp run_multiple_results/result_${BATCH_SIZE}.npz /gluster/home/ofrebato/baler/run_multiple_results/results_${BATCH_SIZE}.npz
+cp Hist_mean_distances.png /gluster/home/ofrebato/baler/hist_mean_distances_${BATCH_SIZE}.png
+cp mean_mean_distances.png /gluster/home/ofrebato/baler/mean_mean_distances_${BATCH_SIZE}.png
+cp All_mean_distances.png /gluster/home/ofrebato/baler/all_mean_distances_${BATCH_SIZE}.png
 
-#copy results
-cp job.out /gluster/home/ofrebato/baler/job.out
-cp job.err /gluster/home/ofrebato/baler/job.err
-cp job.log /gluster/home/ofrebato/baler/job.log
-cp job.all_output /gluster/home/ofrebato/job.all_output
-cp run_multiple_results/results.npz /gluster/home/ofrebato/baler/run_multiple_results/results.npz
-
-cat job.all_output
 echo "COMPLETED RUN_SCRIPT.SH"
